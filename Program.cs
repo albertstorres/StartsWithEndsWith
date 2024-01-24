e2e2c4071d81fdcf90236e5baeaa692f6e9475f0
Albert Sevy <albertstorres@gmail.com>
2024-01-23 21:10:32 -0300

    Finalizando o repositório da aula Strings/StartsWithEndsWith

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StartsWithEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Texto de teste";
+
+            Console.WriteLine(texto.StartsWith("texto"));
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.StartsWith("t"));
+            Console.WriteLine(texto.EndsWith("o"));
+        }
+    }
+}
